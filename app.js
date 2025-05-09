4a0b214d8100a56341b73beeee31b1189a300895
simoneburrai <burrai.s98@gmail.com>
2025-05-09 15:02:08 +0200

    feat: show function reviews added

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 const movieRouter = require("./routers/movieRouter");
 
+app.use(express.static("./public"));
 app.use(express.json());
-
 app.use("/movies", movieRouter);
 
 app.listen(port, () => {
--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,16 +3,38 @@ const connection = require("../data/db");
 const index = (req, res) => {
     const sql = "SELECT * FROM movies"
     connection.query(sql, (error, result) => {
+        if (error) {
+            return res.status(500).json({ error: 'Database query failed' });
+        }
         res.json(result);
     })
 }
 
-const show = (req, res) => {
-    const id = req.params.id;
-    const sql = "SELECT * FROM movies where id = ?"
-    connection.query(sql, [id], (error, result) => {
-        res.json(result);
-    })
+function show(req, res) {
+    const { id } = req.params;
+
+    const movieSql = 'SELECT * FROM movies WHERE id = ?';
+
+    const reviewSql = `SELECT * FROM reviews WHERE movie_id = ?`
+
+    connection.query(movieSql, [id], (err, movieResults) => {
+        if (err) {
+            return res.status(500).json({ error: 'Database query failed' });
+        }
+
+        if (movieResults.length === 0) {
+            return res.status(404).json({ error: 'Movie not found' });
+        }
+
+        const movie = movieResults[0];
+
+        connection.query(reviewSql, [id], (err, reviewResults) => {
+            if (err) {
+                return res.status(500).json({ error: 'Database query failed' });
+            }
+            res.json({ movie, reviews: reviewResults });
+        });
+    });
 }
 
 
